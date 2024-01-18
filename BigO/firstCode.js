0143dea4fa81e33f1a69b98c20910bea67e554ca
vikash-parida <vikashparida067@gmail.com>
2024-01-18 23:44:58 +0530

    create large array uesing new Array method

--- BigO/firstCode.js
+++ BigO/firstCode.js
@@ -1,12 +1,12 @@
 let nemo = ["nemo"];
 let everyone = ['dora','bruce','marlin','gill','bloat','nigel','darla','nemo','hank']
-
+let largeArray = new Array(1000).fill("vikash");
 function findNemo(listOfName){
 
     let t1 = performance.now();
 
     for(let i = 0; i < listOfName.length; i++) {
-        if(listOfName[i] === "nemo"){
+        if(listOfName[i] === "vikash"){
             console.log(listOfName[i])
         }
     }
@@ -17,5 +17,5 @@ function findNemo(listOfName){
 }
 
 
-findNemo(everyone);
+findNemo(largeArray);
 
