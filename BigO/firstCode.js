cb0e6c7ade11904c6ca7dfefd85a7026795b3318
vikash-parida <vikashparida067@gmail.com>
2024-01-18 23:41:27 +0530

    first data Learn BIgO

--- BigO/firstCode.js
+++ BigO/firstCode.js
@@ -3,11 +3,17 @@ let everyone = ['dora','bruce','marlin','gill','bloat','nigel','darla','nemo','h
 
 function findNemo(listOfName){
 
+    let t1 = performance.now();
+
     for(let i = 0; i < listOfName.length; i++) {
         if(listOfName[i] === "nemo"){
             console.log(listOfName[i])
         }
     }
+
+    let t2 = performance.now();
+
+    console.log(`call nemo took ${t2 - t1}  millisecond`);
 }
 
 
