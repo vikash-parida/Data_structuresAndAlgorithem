923e5ed38a1aed33e7b63ef84def86aade3ab562
vikash-parida <vikashparida067@gmail.com>
2024-04-09 23:28:26 +0530

    check performance

--- BigO/firstCode.js
+++ BigO/firstCode.js
@@ -3,7 +3,7 @@ let everyone = ['dora','bruce','marlin','gill','bloat','nigel','darla','nemo','h
 let largeArray = new Array(1000).fill("vikash");
 function findNemo(listOfName){
 
-    // let t1 = performance.now();
+    let t1 = performance.now();
 
     for(let i = 0; i < listOfName.length; i++) {
         if(listOfName[i] === "vikash"){
@@ -11,9 +11,9 @@ function findNemo(listOfName){
         }
     }
 
-    // let t2 = performance.now();
+    let t2 = performance.now();
 
-    // console.log(`call nemo took ${t2 - t1}  millisecond`);
+    console.log(`call nemo took ${t2 - t1}  millisecond`);
 }
 
 
