5604c8b7ce5e173713d66ea1af00949fb54473e6
vikash-parida <vikashparida067@gmail.com>
2024-04-11 23:54:59 +0530

    big O(1)

---
+++ BigO/BigO1.js
@@ -0,0 +1,17 @@
+
+//this is bigO of 1  notaion give n number of input return constent number of output   this is simple type of example .
+let userArrar = [1,2,3,4,5,6,7,8,9,10,11,"n....."];
+
+function printNumberWithLoop(input) {
+    console.log(input[0]);
+    console.log(input[1]);
+    console.log(input[2]);
+    console.log(input[3]);
+    console.log(input[4]);
+    console.log(input[5]);
+    console.log(input[6]);
+    return;
+}
+
+
+printNumberWithLoop(userArrar);
